1228b54c5040ea8bc01b33383329776a98e7c000
Tshepo <Tshepo@LAPTOP-L5QK29F1>
2023-09-06 18:22:03 +0200

    Created the Models and product class, created the Product Repository and Added system.cache on DataMemories

---
+++ Rich-store.Core/Rich-store.Core/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rich_store.Core.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        [StringLength(30)]
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Range(0,50000)]
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+
+        public Product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+    }
+}
