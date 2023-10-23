1bb4b1454afd7c21b61eee24c2338e0723afecae
Tshepo <Tshepo@LAPTOP-L5QK29F1>
2023-10-23 18:23:15 +0200

    Added ProductListVM class

---
+++ Rich-store.Core/Rich-store.Core/View Models/ProductListVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rich_store.Core.View_Models
+{
+    internal class ProductListVM
+    {
+    }
+}
